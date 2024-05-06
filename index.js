eb93e48b95e4fed9fb025bd89746108a7b0f0220
cfdtyghusij <gs1hantouti@hotmail.com>
2024-05-06 10:13:03 +0700

    aptos task

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-const gcdRecursive = (a, b) => {
-  if (b === 0) {
-    return a;
-  }
-  return gcdRecursive(b, a % b);
-};
+function largestNumber(nums) {
+  if (nums.every((num) => num === 0)) return "0";
+  return nums.sort((a, b) => `${b}${a}` - `${a}${b}`).join("");
+}
