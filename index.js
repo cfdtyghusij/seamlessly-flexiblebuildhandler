8ae0a14674ce54145bfdd40076be4bd31fc8e6e1
cfdtyghusij <gs1hantouti@hotmail.com>
2024-05-08 01:37:14 +0700

    update aptos

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-function largestNumber(nums) {
-  if (nums.every((num) => num === 0)) return "0";
-  return nums.sort((a, b) => `${b}${a}` - `${a}${b}`).join("");
+function isAnagram(s, t) {
+  if (s.length !== t.length) return false;
+  const map = new Map();
+  for (const char of s) {
+    map.set(char, (map.get(char) || 0) + 1);
+  }
+  for (const char of t) {
+    if (!map.has(char) || map.get(char) === 0) return false;
+    map.set(char, map.get(char) - 1);
+  }
+  return true;
 }
