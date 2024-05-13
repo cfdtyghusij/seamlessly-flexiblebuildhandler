f5afb1558164493a2d0718887b1bbb1a72130ea0
cfdtyghusij <gs1hantouti@hotmail.com>
2024-05-13 07:48:55 +0700

    feat task

--- index.js
+++ index.js
@@ -1,18 +1,10 @@
-function permute(nums) {
+function generateParenthesis(n) {
   const result = [];
-  backtrack([]);
+  backtrack("", 0, 0);
   return result;
-  function backtrack(permutation) {
-    if (permutation.length === nums.length) {
-      result.push([...permutation]);
-      return;
-    }
-    for (const num of nums) {
-      if (!permutation.includes(num)) {
-        permutation.push(num);
-        backtrack(permutation);
-        permutation.pop();
-      }
-    }
+  function backtrack(current, open, close) {
+    if (current.length === 2 * n) result.push(current);
+    if (open < n) backtrack(current + "(", open + 1, close);
+    if (close < open) backtrack(current + ")", open, close + 1);
   }
 }
